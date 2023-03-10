8fd9533bb94fef8bf111823582246e65d7895c78
aditya <adityatandulkar20@gmail.com>
2023-03-10 16:57:46 +0530

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const LoadingStatus = {
 };
 
 const App = () => {
-  const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
+  const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users/";
   const [userId, setUserId] = React.useState(1);
   const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
   const [userData, setUserData] = React.useState({
@@ -17,10 +17,24 @@ const App = () => {
     email: "",
     name: "",
     phone: "",
-    webiste: "",
+    website: "",
   });
 
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+    setIsLoading(LoadingStatus.IN_PROGRESS);
+    fetch(BASE_URL+userId)
+      .then(resp => resp.json())
+      .then((resp)=>{
+        setUserData({
+          id : resp.id,
+          email : resp.email,
+          name : resp.name,
+          phone : resp.phone,
+          website : resp.website
+        })
+        setIsLoading(LoadingStatus.SUCCESS)
+      });
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
@@ -42,12 +56,19 @@ const App = () => {
       </button>
 
       <div id="data">
-        <h1>Click on the button to get the user</h1>
-        <h4 id="id">{userData.id}</h4>
-        <h4 id="email">{userData.email}</h4>
-        <h4 id="name">{userData.name}</h4>
-        <h4 id="phone">{userData.phone}</h4>
-        <h4 id="website">{userData.website}</h4>
+        {
+          isLoading === LoadingStatus.IN_PROGRESS ?
+            <Loader />
+          :
+            <>
+              <h1>Click on the button to get the user</h1>
+              <h4 id="id">{userData.id}</h4>
+              <h4 id="email">{userData.email}</h4>
+              <h4 id="name">{userData.name}</h4>
+              <h4 id="phone">{userData.phone}</h4>
+              <h4 id="website">{userData.website}</h4>
+            </>
+        }
       </div>
     </div>
   );
